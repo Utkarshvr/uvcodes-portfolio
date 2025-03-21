7a3ad2f1905a4faafd0b3a6755550b703b46c3ea
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-03-21 18:17:36 +0530

    fixed Experience UI Issues

--- src/Screens/Exprience/Exprience.tsx
+++ src/Screens/Exprience/Exprience.tsx
@@ -6,13 +6,13 @@ export default function Exprience({
 }: {
   experience: EXPERIENCE_TYPE[];
 }) {
-  console.log({experience})
+  console.log({ experience });
   return (
     <section
       id="experience"
-      className="md:px-9 h-svh flex flex-col items-center justify-center gap-4"
+      className="md:px-9 px-4 min-h-svh flex flex-col items-center justify-center gap-4"
     >
-      <h1 className="w-full font-bold text-2xl text-start px-4 md:px-0">
+      <h1 className="w-full font-bold text-2xl text-start">
         Experience
       </h1>
 
@@ -20,22 +20,24 @@ export default function Exprience({
         {experience.map((exp) => (
           <RevealOnFrame key={exp.id}>
             <div className="flex flex-col gap-1">
-              <div className="flex gap-2">
+              <div className="flex gap-2 items-center">
                 <img
                   src={exp.icon}
                   className="w-7 h-7 rounded-full"
                   alt={exp.organization}
                 />
-                <h4 className="text-neutral-200 font-bold text-lg">
+                <h4 className="text-slate-200 font-bold text-sm md:text-lg">
                   {exp.organization} | {exp.title}
                 </h4>
               </div>
-              <h5 className="text-neutral-300 font-medium text-md">
+              <h5 className="text-slate-300 font-medium text-xs md:text-sm">
                 {exp.duration}
               </h5>
               {exp.description &&
                 exp.description.map((desc) => (
-                  <p className="text-neutral-400">&#x2022; {desc}</p>
+                  <p className="text-slate-300 text-sm md:text-base">
+                    &#x2022; {desc}
+                  </p>
                 ))}
             </div>
           </RevealOnFrame>
