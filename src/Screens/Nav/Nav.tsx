e718f9be747ceb33a07cde5d7d77933aeb5a9f26
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-03-25 21:47:35 +0530

    nav text size fixed

--- src/Screens/Nav/Nav.tsx
+++ src/Screens/Nav/Nav.tsx
@@ -6,13 +6,13 @@ export default function Nav() {
     <header className="min-h-[8vh] z-10 p-4 bg-slate-950 fixed w-full top-0 left-0 flex items-center justify-center">
       {/* <img src="/logo.png" className="w-12 h-12 rounded-full" /> */}
       <AutoReveal delay={0.7}>
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-1 sm:gap-3 flex-wrap  justify-center">
           {["About", "Experience", "Testimonials", "Work", "Contact"].map(
             (link) => (
               <Link
                 key={link}
                 href={`#${link.toLowerCase()}`}
-                className="text-sm font-bold text-netrual-200 hover:font-medium transition-all"
+                className="text-xs sm:text-sm font-bold text-netrual-200 hover:font-medium transition-all"
               >
                 {link}
               </Link>
